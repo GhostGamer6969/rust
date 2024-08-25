5408381eedd8a356a8dda355a67cb4e564c2a02e
GhostGamer <arambhmani@hotmail.com>
2024-08-26 01:12:17 +0530

    Calculator inf

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,34 @@ fn div(n1:f32,n2:f32)->f32{
 
 fn main(){
     let (mut n1,mut n2,mut s)=(String::new(),String::new(),String::new());
-
     println!("Enter a num: ");
     io::stdin().read_line(&mut n1).expect("Failed to read line");
-    let inp1:f32=n1.trim().parse().expect("Enter valid number");
+    let mut inp1:f32=n1.trim().parse().expect("Enter valid number");
 
-    println!("Enter a num: ");
-    io::stdin().read_line(&mut n2).expect("Failed to read line");
-    let inp2:f32=n2.trim().parse().expect("Enter valid number");
+    loop{
+        n2.clear();
+        s.clear();
 
-    println!("Enter a operation: ");
-    io::stdin().read_line(&mut s).expect("Failed to read line");
-    let opr=s.trim();
-
-    let result = match opr{
-        "+"=>add(inp1,inp2),
-        "-"=>subt(inp1,inp2),
-        "*"=>mul(inp1,inp2),
-        "/"=>div(inp1,inp2),
-        _=>{
-            println!("Invalid operation");
-            return;
+        println!("Enter a operation: ");
+        io::stdin().read_line(&mut s).expect("Failed to read line");
+        let opr=s.trim();
+        if opr == "=" {
+            break;
         }
-    };
-    println!("result {}",result);
+        println!("Enter a num: ");
+        io::stdin().read_line(&mut n2).expect("Failed to read line");
+        let inp2=n2.trim().parse().expect("Enter valid number");
+
+        inp1 = match opr{
+            "+"=>add(inp1,inp2),
+            "-"=>subt(inp1,inp2),
+            "*"=>mul(inp1,inp2),
+            "/"=>div(inp1,inp2),
+            _=>{
+                println!("Invalid operation");
+                return;
+            }
+        };
+    }
+    println!("result {}",inp1);
 }
