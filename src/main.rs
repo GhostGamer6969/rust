a3d435343a18bedb61a02c9a0d7a2bb6bc76b5ac
GhostGamer <arambhmani@hotmail.com>
2024-08-26 00:25:49 +0530

    Basic calcy

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-use std::io::{self,Write};
-fn main(){
-    let mut f = String::new();
-    io::stdout().write(b"Enter the number to factorial: ").unwrap();
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut f).unwrap();
-    let f = f.trim().parse().unwrap();
-    println!("{}",fact(f));
+use std::io::{self};
+
+fn add(n1:f32,n2:f32)->f32{
+    n1+n2
 }
 
-fn fact(f:i32)-> i32{
-    if f == 0{
-        1
-    }else{
-        f*fact(f-1)
-    }
+fn subt(n1:f32,n2:f32)->f32{
+    n1-n2
+}
+
+fn mul(n1:f32,n2:f32)->f32{
+    n1*n2
+}
+
+fn div(n1:f32,n2:f32)->f32{
+    n1/n2
+}
+
+fn main(){
+    let (mut n1,mut n2,mut s)=(String::new(),String::new(),String::new());
+
+    println!("Enter a num: ");
+    io::stdin().read_line(&mut n1).expect("Failed to read line");
+    let inp1:f32=n1.trim().parse().expect("Enter valid number");
+
+    println!("Enter a num: ");
+    io::stdin().read_line(&mut n2).expect("Failed to read line");
+    let inp2:f32=n2.trim().parse().expect("Enter valid number");
+
+    println!("Enter a operation: ");
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+    let opr=s.trim();
+
+    let result = match opr{
+        "+"=>add(inp1,inp2),
+        "-"=>subt(inp1,inp2),
+        "*"=>mul(inp1,inp2),
+        "/"=>div(inp1,inp2),
+        _=>{
+            println!("Invalid operation");
+            return;
+        }
+    };
+    println!("result {}",result);
 }
