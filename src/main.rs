713c5292644e2e1a99b735a4c8da35ab2f5649fc
GhostGamer <arambhmani@hotmail.com>
2024-08-25 14:18:27 +0530

    way 1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use std::io::{self,Write};
-use std::mem::size_of_val;
-fn main() {
-    let mut age=String::new();
-    io::stdout().write(b"Enter your age: ").unwrap();
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut age).unwrap();
-    let age:isize=age.trim().parse().unwrap();
-    if age >= 18{
-        println!("you can vote");
-        print!("{}",size_of_val(&age));
-    }
-    else{
-        println!("you cant vote");
-        print!("{}",size_of_val(&age));
-    }
+fn main(){
+    let v={
+        let mut x:i32 = 1;
+        x += 2;
+        x
+    };
+    assert_eq!(v,3);
+    println!("success");
 }
