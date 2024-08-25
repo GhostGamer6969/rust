13860a0ecd618b1aabf1dfb35ea7467af8b7dbc6
GhostGamer <arambhmani@hotmail.com>
2024-08-25 13:51:30 +0530

    size of val

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io::{self,Write};
+use std::mem::size_of_val;
 fn main() {
     let mut age=String::new();
     io::stdout().write(b"Enter your age: ").unwrap();
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut age).unwrap();
-    let age:i32=age.trim().parse().unwrap();
+    let age:isize=age.trim().parse().unwrap();
     if age >= 18{
         println!("you can vote");
+        print!("{}",size_of_val(&age));
     }
     else{
         println!("you cant vote");
+        print!("{}",size_of_val(&age));
     }
 }
